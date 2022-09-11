584bd7309939727c94e9475d60187c79fd45b2e8
Ekaterina <@example.com>
2022-09-11 17:33:36 +0300

    Таблица умножения

---
+++ Example_001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вывода таблицы умножения на экран.
+
+for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+Console.WriteLine();
+}
